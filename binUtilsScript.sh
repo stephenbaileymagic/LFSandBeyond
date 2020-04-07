8fb9f0404d25c065f3d90c22e2d96124d07ea75e
stephenbaileymagic <stephenbaileymagic@gmail.com>
2020-04-06 18:43:49 -0700

    Add existing file

---
+++ binUtilsScript.sh
@@ -0,0 +1,10 @@
+mkdir -v build
+cd build
+../configure --prefix=/tools            \
+             --with-sysroot=$LFS        \
+             --with-lib-path=/tools/lib \
+             --target=$LFS_TGT          \
+             --disable-nls              \
+             --disable-werror
+make
+make install
